f04c5a6a41b7768acc6a3819308adf86992a8173
Ryan <ryanjoy1221@gmail.com>
2025-11-20 12:28:52 +0000

    initial commit

---
+++ src/main/java/com/example/appdevgproject/AppDevGProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdevgproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDevGProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDevGProjectApplication.class, args);
+    }
+
+}
